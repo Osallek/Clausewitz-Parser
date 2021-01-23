8df5c2c490baeebf562850f617c9f91fb40f8877
GaetanRAYNAUD <gaetanraynaud@outlook.com>
2021-01-23 12:46:29 +0100

    Upgrade to Java 15

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module fr.osallek.clausewitzparser {
+    requires ch.qos.logback.core;
+    requires org.slf4j;
+
+    exports fr.osallek.clausewitzparser.common;
+    exports fr.osallek.clausewitzparser.model;
+    exports fr.osallek.clausewitzparser;
+}
